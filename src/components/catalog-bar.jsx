605b078cceaa11dd613ec9bc92814f214aa77d8c
Yusuf AL <yusufal5558@gmail.com>
2023-09-19 15:31:25 +0300

    added genres

--- src/components/catalog-bar.jsx
+++ src/components/catalog-bar.jsx
@@ -1,13 +1,12 @@
-'use client'
 import React, { useState, useEffect } from 'react';
 import { BiChevronDown } from "react-icons/bi";
 import { useTabs } from '@/app/Context/TabsContext';
 
-
-const CatalogBar = () => {
+const CatalogBar = (props) => {
   const { tabs, setTabs } = useTabs();
   const [genres, setGenres] = useState([]);
-  const [showList, setShowList] = useState(false)
+  const [showList, setShowList] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=9f34a27b7dd55dd1715d36f9f331c3d3')
@@ -25,27 +24,36 @@ const CatalogBar = () => {
       });
   }, []);
 
+  const handleGenreClick = async (genreId) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/${tabs !== 'airing_today' ? 'movie' : 'tv'}/${tabs}?api_key=9f34a27b7dd55dd1715d36f9f331c3d3&with_genres=${genreId}`);
+      const data = await response.json();
+      console.log(data);
+      setSelectedGenre(genreId);
+      props.updateMovies(genreId);
+    } catch (error) {
+      console.error('Filmler alınamadı:', error);
+    }
+  }
+
   return (
     <div className='bg-[#172b4e] p-6 flex justify-between rounded-xl'>
       <ul className='flex items-center gap-7'>
-      <li onClick={() => setShowList((current) => !current)} className='relative flex items-center gap-1 cursor-pointer transition-colors'>
-                <div className='flex gap-2 cursor-pointer hover:text-secondary items-center'>
-                  <span className=''>All Genres</span>
-                  <BiChevronDown/>
-                </div>
-                <div  className={`absolute  bg-[#172b4e] px-4 py-3  min-w-[160px] rounded-xl top-[75px] z-10 ${showList == false ? "hidden" : ""}`}>
-                    <div className='flex flex-col gap-3 max-h-[240px] overflow-y-scroll'>
-                    <ul>
-                      {
-                        genres.map((genre) => (
-                          <li>{genre.name}</li>
-                        ))
-                      }
-                    </ul>
-                    </div>
-                </div>
-                
-            </li>
+        <li onClick={() => setShowList((current) => !current)} className='relative flex items-center gap-1 cursor-pointer transition-colors'>
+          <div className='flex gap-2 cursor-pointer hover:text-secondary items-center'>
+            <span>{selectedGenre !== null ? genres.find((genre) => genre.id === selectedGenre)?.name : 'All genres'}</span>
+            <BiChevronDown/>
+          </div>
+          <div  className={`absolute  bg-[#172b4e] px-4 py-3  min-w-[160px] rounded-xl top-[75px] z-10 ${showList == false ? "hidden" : ""}`}>
+            <div className='flex flex-col gap-3 max-h-[240px] overflow-y-scroll'>
+              <ul>
+                {genres.map((genre) => (
+                  <li key={genre.id} onClick={() => handleGenreClick(genre.id)}>{genre.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </li>
         <li className='flex items-center gap-1 cursor-pointer hover:text-secondary transition-colors'><span>All the years</span> <BiChevronDown/></li>
       </ul>
       <ul className='flex gap-5 items-center bg-primary p-2 rounded-xl font-medium'>
--- src/components/main-section.jsx
+++ src/components/main-section.jsx
@@ -13,7 +13,7 @@ import CatalogBar from './catalog-bar'
 const MainSection = () => {
   const { tabs } = useTabs();
   // const { genres, selectedGenre } = useGenres();
-  const [topMovies, setTopMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [movieDetail, setMovieDetail] = useState(false)
   const [hoverStates, setHoverStates] = useState([]);
 
@@ -31,24 +31,35 @@ const MainSection = () => {
         `https://api.themoviedb.org/3/${tabs !== 'airing_today' ? 'movie' : 'tv'}/${tabs}?api_key=9f34a27b7dd55dd1715d36f9f331c3d3`
       );
       const movieData = await response.json();
-      setTopMovies(movieData.results);
+      setMovies(movieData.results);
     };
 
     fetchMovies();
   }, [tabs]);
 
+  const updateMovies = async (genreId) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/${tabs !== 'airing_today' ? 'movie' : 'tv'}/${tabs}?api_key=9f34a27b7dd55dd1715d36f9f331c3d3&with_genres=${genreId}`);
+      const movieData = await response.json();
+      setMovies(movieData.results);
+    } catch (error) {
+      console.error('Filmler alınamadı:', error);
+    }
+  };
+  
+
   return (
         <div className='px-[10%] bg-primary text-white pt-[5%] pb-[2%]'>
-        <CatalogBar/>
+        <CatalogBar updateMovies={updateMovies} />
             <div className='grid grid-cols-4 gap-10 mt-8 relative'>
-              {topMovies.map((topMovie, index) => (
-                    <div onClick={() => router.push(`/movie/${topMovie.id}`)} className='flex flex-col gap-3' key={topMovie.id}>
+              {movies.map((movie, index) => (
+                    <div onClick={() => router.push(`/movie/${movie.id}`)} className='flex flex-col gap-3' key={movie.id}>
                         <div onMouseEnter={() => toggleHover(index)} onMouseLeave={() => toggleHover(index)} className="relative cursor-pointer overflow-hidden rounded-2xl bg-gray-500 opacity-70 hover:bg-transparent hover:opacity-100 transition duration-500">
                           {
-                            topMovie?.backdrop_path == null ?
+                            movie?.backdrop_path == null ?
                             <div className='min-h-[193.16px]'>
                             </div> : 
-                            <img src={`https://image.tmdb.org/t/p/original${topMovie?.backdrop_path}`} alt="" className="object-cover relative hover:blur-sm hover:scale-110 transition duration-500 cursor-pointer"/>
+                            <img src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`} alt="" className="object-cover relative hover:blur-sm hover:scale-110 transition duration-500 cursor-pointer"/>
                           }
                           <div className={`${hoverStates[index] ? "" : "hidden"} transition duration-500`}>
                               <div className="absolute top-5 left-5 bg-primary p-2 rounded-lg">
@@ -56,14 +67,14 @@ const MainSection = () => {
                               </div>
                               <div className="absolute top-5 right-5 flex gap-1 bg-primary p-2 rounded-lg">
                                 <AiOutlineStar size={15} className="text-secondary"/>
-                                <span className="text-xs text-white hover:!opacity-100">{topMovie?.vote_average}</span>
+                                <span className="text-xs text-white hover:!opacity-100">{movie?.vote_average}</span>
                               </div>
                               <BsPlayCircle size={40} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'/>
                           </div>
                         </div>
                         <div className='flex justify-between gap-10'>
-                          <p className='font-semibold'>{tabs !== 'airing_today' ? topMovie?.title : topMovie?.name}</p>
-                          <p>{tabs !== 'airing_today' ? moment(topMovie?.release_date).format('YYYY') : ''}</p>
+                          <p className='font-semibold'>{tabs !== 'airing_today' ? movie?.title : movie?.name}</p>
+                          <p>{tabs !== 'airing_today' ? moment(movie?.release_date).format('YYYY') : ''}</p>
                         </div>
                     </div>
               ))}
